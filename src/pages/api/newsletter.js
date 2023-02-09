047a5265a3e138a24b13e41a13558ad34b973eb3
j-dublon <j.johnson.dublon@gmail.com>
2023-02-09 11:22:11 +0000

    Connect newsletter registration to MongoDB database

--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -8,7 +10,16 @@ export default function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const url = `mongodb+srv://fullstack-next:${process.env.MONGODB_PASSWORD}@cluster0.qtrcbub.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(url);
+
+    await client.connect();
+
+    const db = client.db("fullstack-next");
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: "You have been signed up" });
   }
 }
